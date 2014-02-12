7a37836a3d52ec5cc8ac785353838f7c020eb425
sal <sal@redhat.com>
2014-02-12 18:29:05 +0100

    Added integration tests to check for report generation in different apckage types

---
+++ src/it/bmd-it-jar-xml-cmdln/verify.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2006-2013 smartics, Kronseder & Reiner GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+  Requires FIX on Linux:
+    https://git-wip-us.apache.org/repos/asf?p=maven.git;a=commitdiff;h=70218af1c1824caa892e7e507463ea713faa7133
+  in mvn shell script:
+    export MAVEN_CMD_LINE_ARGS="$@"
+
+def fileXml = new File(basedir, 'target/classes/META-INF/buildmetadata.xml')
+assert fileXml.exists()
+
+
+def buildmetadata = new XmlSlurper().parse(fileXml)
+
+def cmdline = buildmetadata.runtime.maven.commandline.text()
+assert cmdline.contains("clean package");
+ */
